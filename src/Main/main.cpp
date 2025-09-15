9ed3b84223fe56368811b32404f6452692621f27
ulisesmedina3 <ulises.medina3@upr.edu>
2025-09-15 19:00:27 -0400

    gitignore update

--- src/Main/main.cpp
+++ src/Main/main.cpp
@@ -8,10 +8,13 @@ FSM fsm(myServo);  // Pass the Servo reference to FSM
 
 void setup() {
   Serial.begin(115200);
+  delay(10000);
+  Serial.println("ESP32-S3 Serial working setup");
   myServo.attach(18);  // Attach to a valid PWM pin
 }
 
 void loop() {
   fsm.handle();
-  delay(100);
+  Serial.println("ESP32-S3 Serial working");
+  delay(10000);
 }
