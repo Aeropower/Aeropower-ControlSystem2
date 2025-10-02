47b79ce3b35dcd5ccd2e701ada1b61fcf0a6cb8e
hiramrodriguez14 <hiram.rodriguez14@upr.edu>
2025-10-01 21:56:42 -0400

    Added some comments and changed state transition (if) arguments

--- src/Main/fsm.cpp
+++ src/Main/fsm.cpp
@@ -1,4 +1,4 @@
-#include "./../../include/fsm.h"
+#include "fsm.h"
 /*This FSM machine changes the turbine state based on the wind speed read from
   the Modern Devices Rev. C anemometer*/
 #include <Arduino.h>
@@ -6,12 +6,17 @@
 #include "telemetry.h"
 // Example wind speeds, the real ones must be calculated with mechanical
 // division
-#define CUT_IN_SPEED 0 //Example: 5 m/s
-#define RATED_SPEED 0
-#define CUT_OUT_SPEED 0
+#define CUT_IN_SPEED 2  // Example: 2 m/s; below that turbine is on stall
+#define RATED_SPEED  4
+   // Example: 4 m/s below that and greater than CUT_IN_SPEED turbine is on
+   // MPPT(torque)
+#define CUT_OUT_SPEED 6
+   // Example: 6 m/s; below that turbine is on pitch control, greater than
+   // that turbine must enter on emergency mode
 
-FSM::FSM(Servo& bladesServo)
-    : pitchControlState(bladesServo),  // Initialize pitchControlState correctly
+   //The FSM constructor initializes the states objects and passes them the servo
+    FSM::FSM(Servo& bladesServo)
+    : pitchControlState(bladesServo),  
       stallState(bladesServo),
       torqueControlState(bladesServo),
       emergencyStopState(bladesServo) {
@@ -19,9 +24,7 @@ FSM::FSM(Servo& bladesServo)
   currentState->onEnter();
 }
 
-/* Hay que verificar esto, no estoy seguro de tenerlo bien
-   like, no se si siempre sera asi o la logica cambia dependiendo del estado en
-   el que se encuentre*/
+
 void FSM::handle() {
   if (!currentState) return;
 
@@ -35,22 +38,22 @@ void FSM::handle() {
 
   // Decide next state
   State* next = nullptr;
-  if (windSpeed == CUT_IN_SPEED) {
+  if (windSpeed < CUT_IN_SPEED) {
     next = &stallState;
-    Serial.println("FSM: switching to Stall"); 
+    Serial.println("FSM: switching to Stall");
   } else if (windSpeed < RATED_SPEED) {
     next = &torqueControlState;
-    Serial.println("FSM: switching to Torque"); 
-  } else if (windSpeed > CUT_OUT_SPEED) { ///// <
+    Serial.println("FSM: switching to Torque");
+  } else if (windSpeed < CUT_OUT_SPEED) {  ///// <
     next = &pitchControlState;
-    Serial.println("FSM: switching to PitchControlState"); 
+    Serial.println("FSM: switching to PitchControlState");
   } else {
     next = &emergencyStopState;
   }
 
   // Transition if state changed
   if (next != currentState) {
-    Serial.println("FSM: Actual swicth"); 
+    Serial.println("FSM: Actual swicth");
     currentState->onExit();
     currentState = next;
     currentState->reset();
